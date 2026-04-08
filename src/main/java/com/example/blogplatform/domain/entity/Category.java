f6729d50b1c79eac15be5290d7c2ff07367901d1
Sovetov_YaV <sovetovyaroslav431@gmail.com>
2026-04-09 01:50:29 +0300

    add category entity with repository

---
+++ src/main/java/com/example/blogplatform/domain/entity/Category.java
@@ -0,0 +1,31 @@
+package com.example.blogplatform.domain.entity;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+}
