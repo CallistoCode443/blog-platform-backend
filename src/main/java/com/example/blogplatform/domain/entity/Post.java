6e486011f05538b67774ed45c55a066e80c6aa88
Sovetov_YaV <sovetovyaroslav431@gmail.com>
2026-04-09 02:01:26 +0300

    add post entity with repository, and enum PostStatus

---
+++ src/main/java/com/example/blogplatform/domain/entity/Post.java
@@ -0,0 +1,63 @@
+package com.example.blogplatform.domain.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.example.blogplatform.domain.PostStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private PostStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.updatedAt = now;
+        this.createdAt = now;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/example/blogplatform/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.example.blogplatform.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.blogplatform.domain.entity.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, UUID> {
+
+}
