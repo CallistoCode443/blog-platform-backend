c7678147516ffae7cf29d828edd670f8db7e06ea
Sovetov_YaV <sovetovyaroslav431@gmail.com>
2026-04-10 04:10:54 +0300

    update user entity with migration, now username is unique

--- src/main/java/com/example/blogplatform/domain/entity/User.java
+++ src/main/java/com/example/blogplatform/domain/entity/User.java
@@ -36,7 +36,7 @@ public class User {
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
     @Column(name = "password", nullable = false)
