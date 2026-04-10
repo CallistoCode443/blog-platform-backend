560f2c8e5997fbaa49f39e4e379ae6279bb30514
Sovetov_YaV <sovetovyaroslav431@gmail.com>
2026-04-10 04:11:42 +0300

    add secutiry filter chain and authentication manager beans

--- src/main/java/com/example/blogplatform/config/SecurityConfig.java
+++ src/main/java/com/example/blogplatform/config/SecurityConfig.java
@@ -2,13 +2,37 @@ package com.example.blogplatform.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
     }
 }
