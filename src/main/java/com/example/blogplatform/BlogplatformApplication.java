6b3bc57b8fe43c267b99a23be1722dc354346440
Sovetov_YaV <sovetovyaroslav431@gmail.com>
2026-04-11 03:00:59 +0300

    enable configuration properties for CookieProperties

--- src/main/java/com/example/blogplatform/BlogplatformApplication.java
+++ src/main/java/com/example/blogplatform/BlogplatformApplication.java
@@ -2,8 +2,12 @@ package com.example.blogplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.blogplatform.property.CookieProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CookieProperties.class)
 public class BlogplatformApplication {
 
 	public static void main(String[] args) {
